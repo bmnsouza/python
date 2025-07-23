109a2954fddf9bed595f01cba00e817b291d2e9a
Bruno Marcel <bmnsouza@gmail.com>
2025-07-23 17:50:01 -0300

    Adição de indicadores de ETFs

--- web-scraping/atualizar-indicadores/Investimento.py
+++ web-scraping/atualizar-indicadores/Investimento.py
@@ -15,12 +15,46 @@ driver = webdriver.Chrome(options=options)
 print("")
 
 # Abre a planilha
-excel_path = "atualizar-indicadores-fiis.xlsx"
+excel_path = "Investimento.xlsx"
 workbook = load_workbook(excel_path)
 worksheet = workbook["Indicadores"]
 
-# Lê os ativos a partir da célula B3
+# Lê os ETFs a partir da célula B3
+print("*** ETFs ***")
 row = 3
+while True:
+    ativo = worksheet.cell(row = row, column = 2).value
+    if not ativo:
+        break
+
+    url = f"https://investidor10.com.br/etfs-global/{ativo.lower()}/"
+    driver.get(url)
+    time.sleep(1)
+
+    try:
+        # Obtêm os elementos Preço e Dividendo
+        preco = driver.find_element(By.XPATH, "//div[@class='_card cotacao']//div[@class='_card-body']/div[@class='etfCurrentQuotation']/span[1]").text.strip()
+        dividendo = driver.find_element(By.XPATH, "//div[@class='_card dy']//div[@class='_card-body']/span[1]").text.strip()
+        
+        # Imprime os valores dos elementos
+        print(f"{ativo} = Preço: {preco}; Dividendo: {dividendo}")
+
+        # Escreve na planilha
+        worksheet.cell(row = row, column = 3).value = float(preco.replace("US$", "").replace(".", "").replace(",", ".").strip())
+        worksheet.cell(row = row, column = 5).value = float(dividendo.replace("%", "").replace(",", ".").strip()) / 100
+    except Exception as e:        
+        print(f"Erro ao buscar {ativo}: {e}")
+        log = open("Log.txt", "w")
+        log.write(str(e))
+        log.close()
+        break
+    
+    row += 1
+
+# Lê os FIIs a partir da célula B10
+print("")
+print("*** FIIs ***")
+row = 10
 while True:
     ativo = worksheet.cell(row = row, column = 2).value
     if not ativo:
